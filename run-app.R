6d86954b11d82c047d9cbd0b8431661efe7c1d0d
mswang1984 <mingsen.wang@icloud.com>
2019-06-16 18:28:48 -0500

    Changes to be committed: 	modified:   run-app.R

--- run-app.R
+++ run-app.R
@@ -1,6 +1,6 @@
 require(shiny)
 runApp(
-    '~/Dropbox/my-git/monthly-payment-calculator', 
+    '~/Dropbox/my-git/house-hunting-dashboard/', 
     launch.browser = F,
     host = "0.0.0.0",
     port = 4242
